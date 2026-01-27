76a21006ccbf38ed9060b7da7b22e4ea1147e52d
unknown <lider.sistemas@cmmcolombia.org>
2026-01-27 11:00:38 -0500

    login modified 3

--- src/services/jwt.js
+++ src/services/jwt.js
@@ -11,12 +11,16 @@ const secret = process.env.SECRET_KEY;
 // Unix: segundos transcurridos desde el 1 de enero de 1970 hasta hoy.
 const createToken = (user) => {
   const payload = {
-    userId: user._id,
-    role: user.role,
+    userId: user.id_usuario,
+    nombre: user.nombre,
+    email: user.email,
     iat: moment().unix(), // fecha de emisión
     exp: moment().add(7, 'days').unix() // fecha de expiración
   }
 
+  if (!secret) {
+    throw new Error("La clave secreta para el JWT no está definida.");
+  }
   // Devolver el jwt token codificado
   return jwt.encode(payload, secret);
 };

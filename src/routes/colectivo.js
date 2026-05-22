7b15912eece848d79f84c195d2576961fc58757c
unknown <lider.sistemas@cmmcolombia.org>
2026-05-22 11:25:54 -0500

    Ajuste colectivos ampliada

--- src/routes/colectivo.js
+++ src/routes/colectivo.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { ensureAuth } from "../middleware/auth.js";
-import { basicRegisterColectivo, getColectivosXML, searchColectivo, getKoboDataByColectivo } from '../controllers/colectivo.js';
+import { basicRegisterColectivo, getColectivosXML, searchColectivo, getKoboDataByColectivo, extendRegisterColectivo } from '../controllers/colectivo.js';
 
 
 const router = express.Router();
@@ -8,7 +8,8 @@ const router = express.Router();
 router.get('/searchColectivo', searchColectivo);
 router.post('/addColectivo', basicRegisterColectivo);
 router.get('/application/xml', getColectivosXML);
-router.get('/kobo-data/:id_colectivo', getKoboDataByColectivo)
+router.get('/kobo-data/:id_colectivo', getKoboDataByColectivo);
+router.get('/addExtendColectivo', extendRegisterColectivo);
 
 
 
